0d684b93a01586c40bc7b1931a6cf14c25b44cb3
Unknown <sh_ibo@live.com>
2018-12-10 06:29:35 +0800

    wip

--- app/src/main/java/chia/androidassignment/LandingPage.kt
+++ app/src/main/java/chia/androidassignment/LandingPage.kt
@@ -2,43 +2,88 @@ package chia.androidassignment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
+
+
 import android.widget.*
-import kotlinx.android.synthetic.main.activity_landing_page.*
-import kotlinx.android.synthetic.main.activity_movie_detail.*
+
 import android.view.*
-import android.widget.*
+
 import android.content.Intent
+import android.widget.LinearLayout
+
+
+
 
 class LandingPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
-        registerForContextMenu(addMovie)
 
-    }
 
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        if(v?.id == R.id.addMovie){
-            menu?.add(1,1001,1,"Add")
+//        val newMovie = intent.getSerializableExtra("newMovie") as Movie
+
+
+        for( i in 0 .. 5) {
+            val relativeP = RelativeLayout(this)
+            relativeP.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 180)
+
+            val iv = ImageView(this)
+            iv.setImageResource(R.drawable.movie)
+            val layoutParams = LinearLayout.LayoutParams(150, 150)
+            iv.layoutParams = layoutParams
+
+            val tv2 = EditText(this)
+            val lptv1 = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            tv2.setLayoutParams(lptv1)
+            tv2.setText("Avengers")
+            tv2.gravity = Gravity.CENTER
+            tv2.setTextSize(16f)
+            tv2.isEnabled = false
+            val id = 100 + i
+            tv2.setId(id)
+
+            relativeP.removeAllViews()
+            relativeP.addView(iv)
+            relativeP.addView(tv2)
+
+            val finalParent = this.findViewById(R.id.rootLayout) as ViewGroup
+
+            finalParent.addView(relativeP)
+
+
         }
+
+
+
     }
 
-    override fun onContextItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == 1001){
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.land,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-            val intent = Intent(this, MovieRater::class.java)
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.addNew){
+            val movieL: MutableList<Movie> = mutableListOf()
+            val testM = Movie("Venom","OverView","English","19-10-2018",true,true,false)
+            movieL.add(testM)
+            var testing = movieL[0]
+
+
+            val intent = Intent(this, MovieDetail::class.java)
+            var thetest = arrayListOf<Movie>()
+            thetest.add(testM)
+            Toast.makeText(this,thetest[0].title,Toast.LENGTH_LONG).show()
+            intent.putExtra("test",thetest)
             startActivity(intent)
+
+
         }
-
-
-
-        return super.onContextItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
+
+
 
 
 }
